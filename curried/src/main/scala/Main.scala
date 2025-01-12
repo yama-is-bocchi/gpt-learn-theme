346e5b91238f044f2626258c275d14ee67b92578
yamaa <yamaa.com987654321@outlook.jp>
2025-01-12 16:27:56 +0000

    add curried

--- curried/src/main/scala/Main.scala
+++ curried/src/main/scala/Main.scala
@@ -1,5 +1,10 @@
-@main def hello(): Unit =
-  println("Hello world!")
-  println(msg)
-
-def msg = "I was compiled by Scala 3. :)"
+@main def main(): Unit = {
+  val add = { (x: Int, y: Int) => x + y }
+  val multiply = { (x: Int, y: Int, z: Int) => x * y * z }
+  val addCurriedFive = add.curried(5)
+  println(addCurriedFive(-5)) // expect 0
+  println(addCurriedFive(5)) // expect 10
+  val multiplyCurried3 = multiply.curried(3)
+  println(multiplyCurried3(3)(3)) // expect 27
+  println(multiplyCurried3(1)(1)) // expect 3
+}
