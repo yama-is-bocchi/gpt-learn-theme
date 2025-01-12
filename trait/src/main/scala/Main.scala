692468052f5e67bfe692f74603b1d99831aa1d40
yamaa <yamaa.com987654321@outlook.jp>
2025-01-12 14:53:35 +0000

    add trait

--- trait/src/main/scala/Main.scala
+++ trait/src/main/scala/Main.scala
@@ -1,5 +1,27 @@
-@main def hello(): Unit =
-  println("Hello world!")
-  println(msg)
+trait Animal {
+  def speak(): String
+}
 
-def msg = "I was compiled by Scala 3. :)"
+trait HasTail {
+  def tailLength(): String
+}
+
+class Dog extends Animal with HasTail {
+  def speak(): String = "Woof!"
+  def tailLength(): String = "Has a tail of 5cm"
+}
+class Cat extends Animal with HasTail {
+  def speak(): String = "Meow!"
+  def tailLength(): String = "Has a tail of 10cm"
+}
+
+def getHasTailAnimalInformation(animal: Animal & HasTail): String = {
+  s"This animal's speak is ${animal.speak()}, ${animal.tailLength()}"
+}
+
+@main def main(): Unit = {
+  // *** Dogの情報 ***
+  println(getHasTailAnimalInformation(new Dog()))
+  // *** Catの情報 ***
+  println(getHasTailAnimalInformation(new Cat()))
+}
