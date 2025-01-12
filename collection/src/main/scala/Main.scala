79a9be12e9bff03cc1393274f1bd349795a82486
yamaa <yamaa.com987654321@outlook.jp>
2025-01-12 10:01:57 +0000

    add list

--- collection/src/main/scala/Main.scala
+++ collection/src/main/scala/Main.scala
@@ -1,5 +1,12 @@
-@main def hello(): Unit =
-  println("Hello world!")
-  println(msg)
+@main def main(): Unit = {
+  // *** リスト操作 ***
+  // 任意の整数のリストを作成。
+  val numList = List(1, 2, 3, 4, 5)
+  // 各要素を2倍にする新しいリストを生成。
+  println(numList.flatMap[Int](num => List(num * 2)))
+  // 偶数のみをフィルタリングしたリストを生成。
+  println(numList.filter(num => num % 2 == 0))
+  // リストのすべての要素の合計を計算。
+  println(numList.sum)
 
-def msg = "I was compiled by Scala 3. :)"
+}
