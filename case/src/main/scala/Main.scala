b72b3fe5bd89eb1bd4d70ab4e9e59fb7dc184e28
yamaa <yamaa.com987654321@outlook.jp>
2025-01-13 15:14:47 +0000

    add case

--- case/src/main/scala/Main.scala
+++ case/src/main/scala/Main.scala
@@ -1,5 +1,20 @@
-@main def hello(): Unit =
-  println("Hello world!")
-  println(msg)
+sealed trait Shape {
+  def area: Double
+}
 
-def msg = "I was compiled by Scala 3. :)"
+case class Circle(radius: Double) extends Shape {
+  override def area: Double = radius * radius * math.Pi
+}
+
+case class Rectangle(width: Double, height: Double) extends Shape {
+  override def area: Double = width * height
+}
+
+def calcShapeListArea(shapeList: List[Shape]): Double = {
+  shapeList.map(shape => shape.area).sum
+}
+
+@main def main(): Unit = {
+  val shapeList = List[Shape](Circle(5), Rectangle(10, 10), Circle(1))
+  println(calcShapeListArea(shapeList))
+}
